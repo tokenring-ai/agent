c30d6b0988d25fbaa012b04d4e13332133602e2d
Mark Dierolf <mdierolf@cloudquote.io>
2025-08-30 12:33:31 -0700

    Remove `/resources` command and related functionality from chat system

--- chatCommands.ts
+++ chatCommands.ts
@@ -3,6 +3,5 @@ export * as help from "./commands/help.ts";
 export * as hooks from "./commands/hook.ts";
 export * as instructions from "./commands/instruction.ts";
 export * as persona from "./commands/persona.ts";
-export * as resources from "./commands/resource.ts";
 export * as settings from "./commands/settings.ts";
 export * as tools from "./commands/tool.ts";
